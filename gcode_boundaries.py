c3a92e88dd44eb48a4cf38412fb6c38c6bc1cdeb
Kieran Aponte <rilakumafan@gmail.com>
2023-09-04 08:34:11 -0700

    Modified output for clarity
    Removed allow_edge_x & y, changed "Clearance" locations to "Tool/Collet Nut" locations for clarity since it's regarding coordinates

--- gcode_boundaries.py
+++ gcode_boundaries.py
@@ -9,18 +9,16 @@ This software is licensed under the MIT License.
 
 # Units are arbitrary as long as you're consistent between these values and your G Code
 
-path = 'G:\\My Drive\\NC Files\\Lathe Adapter V2\\6.5mm Stock\\Speed Tiger 6.35mm - Lathe Router Adapter 6.5mm Stock.nc'
+path = 'G:\\My Drive\\NC Files\\Lathe Adapter V2\\6.34mm Stock Test\\Speed Tiger 6.35mm - 6.34mm Steel Test Cut.nc'
 max_tool_diameter = 6.35
 max_collet_nut_diameter = 34
 
-# My stock is rectangular with the longest edge along the y axis with the clamps at the top & bottom edges
-allow_edge_x = True  # True if you're okay with the tool center hitting the left/right edges, such as if the clamps are along the top & bottom edges
-allow_edge_y = False # True if you're okay with the tool center hitting the top/bottom edges, such as if the clamps are along the left & right edges
- 
+
+
 max_tool_diameter = max(max_tool_diameter, max_collet_nut_diameter) #Assume collet nut will otherwise collide with clamp at some Z
 
-print('WARNING: This script does not fully account for G2/G3 movements. Use this script with caution.')
-print('If g code was generated in Fusions 360, you may want to temporarily remove the G28 G91 X0 Y0 line at the bottom of your file for an accurate reading.')
+print('WARNING: This script does not fully account for G2/G3 movements. Use with caution.\n')
+print('If G Code includes a synchronization line (such as G28 G91 X0 Y0 as generated from Fusions 360), you may want to temporarily remove it for an accurate reading.\n')
 
 with open(path, 'r') as infile:
     x_min = None
@@ -46,7 +44,7 @@ with open(path, 'r') as infile:
                                 x_min = value
                             if x_max == None or value > x_max: # not using elif in case of single x value in g code
                                 x_max = value
-                        else: # positive value, or separated, such as X 1.618
+                        else: # positive value, or separated by space, such as X 1.618
                             value = float(line[i+1]) # get the numerical value of the full next split item
                             
                             if x_min == None or value < x_min:
@@ -58,7 +56,7 @@ with open(path, 'r') as infile:
                             
                             
                     if item[0].upper() == 'Y':
-                        if len(item) > 1: # negative value, or clustered together, such as Y-1.618
+                        if len(item) > 1: # negative value, or clustered together, such as Y1.618
                             try:
                                 value = float(item[1:])
                             except:
@@ -67,7 +65,7 @@ with open(path, 'r') as infile:
                                 y_min = value
                             if y_max == None or value > y_max: # not using elif in case of single y value in g code
                                 y_max = value
-                        else: # positive value, or separated, such as Y 1.618
+                        else: # positive value, or separated by space, such as Y 1.618
                             value = float(line[i+1]) # get the numerical value of the full next split item
                             
                             if y_min == None or value < y_min:
@@ -86,11 +84,16 @@ with open(path, 'r') as infile:
                     
         
 tool_radius = max_tool_diameter / 2 
-tool_radius_x = max_tool_diameter/2 if allow_edge_x == False else 0
-tool_radius_y = max_tool_diameter/2 if allow_edge_y == False else 0
 
 
-print('Clearance X min: {}'.format(x_min - tool_radius_x))
-print('Clearance X max: {}'.format(x_max + tool_radius_x))
-print('Clearance Y min: {}'.format(y_min - tool_radius_y))
-print('Clearance Y max: {}'.format(y_max + tool_radius_y))
+print('Tool Center X min:     {}'.format(x_min))
+print('Tool Center X max:     {}'.format(x_max))
+print('Tool Center Y min:     {}'.format(y_min))
+print('Tool Center Y max:     {}\n'.format(y_max))
+
+
+    
+print('Tool/Collet Nut X min: {}'.format(x_min - tool_radius))
+print('Tool/Collet Nut X max: {}'.format(x_max + tool_radius))
+print('Tool/Collet Nut Y min: {}'.format(y_min - tool_radius))
+print('Tool/Collet Nut Y max: {}'.format(y_max + tool_radius))
